183dbe76ce0f4409ba9ae27764289d0de2683148
JacobAndrewMcclure <jacobmcclure801@gmail.com>
2018-04-19 21:38:22 -0600

    inserted comments

--- src/whatsupworld/WhatsUpWorld.java
+++ src/whatsupworld/WhatsUpWorld.java
@@ -8,13 +8,17 @@ public class WhatsUpWorld
 
     public static void main(String[] args)
     {
+        // initialize variable
         String userName;
         
+        // scanner for user input
         Scanner sc = new Scanner(System.in);
         
+        // system prompt for user name
         System.out.println("Enter your name: ");
         userName = sc.nextLine();
         
+        // print out with user name and welcome message
         System.out.println("Whats up " + userName + "! Welcome to Java!");
     }
     
